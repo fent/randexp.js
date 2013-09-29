90c93cab589649dc299ef8272a072d18802ac3d3
Roly Fentanes <roly426@gmail.com>
2013-09-29 00:13:09 -0400

    just the style

--- test/main-test.js
+++ test/main-test.js
@@ -1,7 +1,7 @@
-var tests = require('./tests.js')
-  , assert = require('assert')
-  , RandExp = require('..')
-  , randexp = require('..').randexp
+var tests = require('./tests.js');
+var assert = require('assert');
+var RandExp = require('..');
+var randexp = require('..').randexp;
 
 
 var match = function(regexp, str, bad) {
@@ -9,39 +9,40 @@ var match = function(regexp, str, bad) {
             (bad ? 'matches' : 'does not match') + ' ' +
             'regexp \'' + regexp.source + '\'';
   var t = regexp.test(str);
-  assert.ok(!bad !== !t, err);
+  assert.ok(bad !== t, err);
 };
+
+function createIt(test) {
+  return  function() {
+    var regs = test.regexp;
+    if (!Array.isArray(regs)) { regs = [regs]; }
+
+    for (var i = 0, l = regs.length; i < l; i++) {
+      var reg = regs[i];
+      var rand = new RandExp(reg);
+
+      // Generate several times.
+      for (var k = 0; k < 5; k++) {
+        match(reg, rand.gen(), test.bad);
+        match(reg, randexp(reg), test.bad);
+      }
+    }
+  };
+}
+
+function createDescribe(test) {
+  return function() {
+    for (var row in test) {
+      if (test.hasOwnProperty(row)) {
+        var t = test[row];
+        it(t.desc, createIt(t));
+      }
+    }
+  };
+}
 
 for (var type in tests) {
   if (tests.hasOwnProperty(type)) {
-    describe(type, function() {
-
-    for (var row in tests[type]) {
-      if (tests[type].hasOwnProperty(row)) {
-        (function(test) {
-
-          it(test.desc, function() {
-            var regs = test.regexp;
-            if (!Array.isArray(regs)) { regs = [regs]; }
-
-            for (var i = 0, l = regs.length; i < l; i++) {
-              var reg = regs[i];
-              var rand = new RandExp(reg);
-              var s, err;
-
-              // generate several times
-              for ( var k = 0; k < 5; k++) {
-                match(reg, rand.gen(), test.bad);
-                match(reg, randexp(reg), test.bad);
-              }
-            }
-
-          });
-
-        })(tests[type][row]);
-      }
-    }
-
-    });
+    describe(type, createDescribe(tests[type]));
   }
 }
