f379dd12246ca964905b2454c70c17810a2c1901
David Tudury <david.tudury@gmail.com>
2014-09-18 22:20:31 -0700

    moved defaulted things to the prototype to be overridden on instances

--- test/custom-range-test.js
+++ test/custom-range-test.js
@@ -5,14 +5,21 @@ var RandExp = require('..');
 
 describe('Modify Range', function() {
   it('Should generate unicode when we expand its range', function() {
-    RandExp.defaultRange.add(0,65535);
-    var output = RandExp.randexp(/.{100}/);
+    var re = new RandExp(/.{100}/);
+    re.defaultRange.add(0,65535);
+    var output = re.gen();
     var max_char = 0;
     for (var i = 0; i < output.length; i++) {
       max_char = Math.max(max_char, output.charCodeAt(i));
     }
     assert.ok(max_char > 127, 'non-ascii characters should have been generated');
-    RandExp.defaultRange.subtract(0, 65535);
-    RandExp.defaultRange.add(32, 126);
+
+    re = new RandExp(/.{100}/);
+    output = re.gen();
+    max_char = 0;
+    for (var i = 0; i < output.length; i++) {
+      max_char = Math.max(max_char, output.charCodeAt(i));
+    }
+    assert.ok(max_char <= 127, 'ascii characters should have been generated (there shouldn\'t be side-effects');
   });
 });
