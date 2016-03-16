b137296122ecdb02876db7901539ce4977818576
fent <933490+fent@users.noreply.github.com>
2016-03-16 02:44:19 -0400

    lookahead... :(

--- test/util-test.js
+++ test/util-test.js
@@ -1,8 +1,34 @@
-var assert = require('assert');
-var ret    = require('ret')
-var util   = require('../lib/util')
+var assert = require('assert-diff');
+var ret    = require('ret');
+var types  = ret.types;
+var util   = require('../lib/util');
 
 
-describe('preprocess', function() {
+describe('Preprocess tokens', function() {
+	describe('No groups', function() {
+		it('Returned tokens match original', function() {
+			var original = ret('hello');
+			var result = util.preprocessGroup(original);
+			assert.deepEqual(original, result);
+		});
+	});
 
+	describe('Group without followed by', function() {
+		it('Returned tokens match original', function() {
+			var original = ret('a(b)');
+			var result = util.preprocessGroup(original);
+			assert.deepEqual(original, result);
+		});
+	});
+
+	describe('Followed by', function() {
+		it('Returns token that match the ones following', function() {
+			var original = ret('(?=a).');
+			var result = util.preprocessGroup(original);
+			assert.deepEqual(result.stack, [{
+				type: types.CHAR,
+				value: 'a'.charCodeAt(0),
+			}]);
+		});
+	});
 });
